bb6a69e728f37e2476f37d7aa24171c524b93a16
Martin Jan Köhler <info@martinjankoehler.com>
2025-09-29 17:28:37 +0200

    dataclass_dict_helpers: support Unions

--- python/klayout_plugin_utils/dataclass_dict_helpers.py
+++ python/klayout_plugin_utils/dataclass_dict_helpers.py
@@ -17,25 +17,50 @@
 #--------------------------------------------------------------------------------
 
 from dataclasses import dataclass, asdict, fields, is_dataclass
-from typing import Dict, get_type_hints
+from pathlib import Path
+from typing import Dict, Union, get_origin, get_type_hints, get_args
 
 
 #--------------------- dataclass serializiation ---------------------------
 def dataclass_from_dict(cls, data: Dict):
-    if not is_dataclass(cls):
-        return data
-    kwargs = {}
-    hints = get_type_hints(cls)
-    for f in fields(cls):
-        if f.name in data:
+    origin = get_origin(cls)
+    args = get_args(cls)
+
+    if origin is Union:  # handle Union types
+        last_error = None
+        for arg in args:
+            try:
+                return dataclass_from_dict(arg, data)
+            except Exception as e:
+                last_error = e
+        raise TypeError(f"Cannot match Union type {cls} with data {data}") from last_error
+
+    elif origin is list:  # handle lists
+        item_type = args[0]
+        return [dataclass_from_dict(item_type, v) for v in data]
+
+    elif is_dataclass(cls):  # normal dataclass
+        if not isinstance(data, dict):
+            raise TypeError(f"Expected dict to instantiate {cls}, got {type(data).__name__}")
+        kwargs = {}
+        hints = get_type_hints(cls)
+        for f in fields(cls):
+            if f.name not in data:
+                raise TypeError(f"Missing field {f.name} for dataclass {cls}")
             field_type = hints[f.name]
-            value = data[f.name]
-            # handle lists of dataclasses
-            origin = getattr(field_type, '__origin__', None)
-            if origin is list:
-                item_type = field_type.__args__[0]
-                value = [dataclass_from_dict(item_type, v) for v in value]
-            else:
-                value = dataclass_from_dict(field_type, value)
+            try:
+                value = dataclass_from_dict(field_type, data[f.name])
+            except Exception as e:
+                raise TypeError(f"Field {f.name} in {cls} failed to parse") from e
             kwargs[f.name] = value
-    return cls(**kwargs)
+        
+        # extra check: make sure data does not contain unexpected keys
+        extra_keys = set(data) - set(f.name for f in fields(cls))
+        if extra_keys:
+            raise TypeError(f"Extra keys {extra_keys} for dataclass {cls}")
+        
+        return cls(**kwargs)
+    elif cls is Path:
+        return Path(data)
+    else:  # primitive type
+        return data
