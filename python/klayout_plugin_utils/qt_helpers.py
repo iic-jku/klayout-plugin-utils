a0681488fb040d4ee98b1212095ada7dd0615f19
Martin Jan Köhler <info@martinjankoehler.com>
2025-11-03 17:52:41 +0100

    Add qt_helpers to fix compatibility issues

---
+++ python/klayout_plugin_utils/qt_helpers.py
@@ -0,0 +1,41 @@
+# --------------------------------------------------------------------------------
+# SPDX-FileCopyrightText: 2025 Martin Jan Köhler
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+# SPDX-License-Identifier: GPL-3.0-or-later
+#--------------------------------------------------------------------------------
+
+from __future__ import annotations
+from typing import *
+import traceback
+
+import pya
+
+
+def qt_major_version() -> int:
+    qt_major = int(pya.Qt.QT_VERSION_STR.split('.')[0])
+    return qt_major
+
+
+def qshortcut(key_sequence: pya.QKeySequence,
+              widget: pya.QWidget,
+              on_trigger: Callable) -> pya.QShortCut:
+    if qt_major_version() >= 6:
+        sc = pya.QShortcut(key_sequence, widget)
+        sc.activated.connect(on_trigger)
+        return sc
+    elif qt_major_version() == 5:
+        return pya.QShortcut(key_sequence, widget, on_trigger)
+    else:
+        raise NotImplementedError()
